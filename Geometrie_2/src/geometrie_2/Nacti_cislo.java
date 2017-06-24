f2a46c9f41af8eff2c867472cfa9772b59088d53
mnovak <marcel.novak14@gmail.com>
2017-06-24 18:13:37 +0200

    Upraveni uzivatelskeho rozhrani do jednotlivych Class, rozdeleni ctyruhelniku na obdelnik a ctverec, Nahrazeni nekde BufferedReaderu Scannerem a osetreni par vstupu

--- Geometrie_2/src/geometrie_2/Nacti_cislo.java
+++ Geometrie_2/src/geometrie_2/Nacti_cislo.java
@@ -6,37 +6,48 @@
 package geometrie_2;
 
 import java.io.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 /**
  *
  * @author mnovak
  */
 public class Nacti_cislo {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static double strana;
-    private double strany[];
-    
-    Nacti_cislo() {   }
     
     static double Nacitani() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            try {
-                        strana = Integer.parseInt(br.readLine());
-                    } catch (NumberFormatException vyj) {
-                          System.out.println("Nespravny format.");
-                           strana = 0;
+        double strana;
+      //  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      Scanner br = new Scanner(System.in);
+      boolean konec = true;
+            do {
+                try {
+                        strana = br.nextDouble();
+                        konec = false;
+                    } catch (InputMismatchException vyj) {
+                        br.next();
+                        System.out.println("Nespravny format.");
+                        strana = 0;
                      }
+            } while ( konec );
             return strana;
     }
     
     double[] Nacitani(int pocet) throws IOException {
-        strany = new double[pocet];
-            try {
-                for (int i = 0; i < strany.length; i ++ )  
-                      strany[i] = Integer.parseInt(br.readLine());
-                    } catch (NumberFormatException vyj) {
-                          System.out.println("Nespravny format.");
-                           strana = 0;
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner br = new Scanner(System.in);
+        double strany[] = new double[pocet];
+        boolean konec = true;
+            do {
+                try {
+                for (int i = 0; i < strany.length; i ++ ) {
+                      strany[i] = br.nextDouble();
+                }
+                      konec = false;
+                    } catch (InputMismatchException vyj) {
+                        br.next();
+                        System.out.println("Nespravny format.");
                      }
+            } while (konec);
             return strany;
     }
 }
